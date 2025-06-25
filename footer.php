9db5b720038c4cf439e720e3d4b085cb5483db09
nguyennhutmc <nguyen.nhutmc@gmail.com>
2025-06-25 08:47:05 +0700

    up

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer-bottom">
+        <p>&copy; 2024 Berlin University Alliance | <a href="#">Impressum</a> | <a href="#">Datenschutz</a></p>
+    </div>
+</footer>
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <meta http-equiv="Expires" content="0">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/import.css" rel="stylesheet"/>
+    <link href="css/style.css" rel="stylesheet"/>
+    <script src="js/jquery.2.2.4.min.js"></script>
+    <title>Hello, world!</title>
+</head>
+<body>
+<div id="page">
+    <header>
+        <nav class="navbar navbar-expand-lg bg-white fixed-top">
+            <div class="container">
+                <a class="logo" href="#">
+                    <img class="logo_img" src="images/logo.svg" alt="">
+                </a>
+                <a class="m-0 p-0 lh-1 mb" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+                    <img class="menu" src="images/menu.svg" alt="">
+                </a>
+                <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar">
+                    <div class="offcanvas-header">
+                        <button type="button" class="btn-close fs-6 ms-auto me-0" data-bs-dismiss="offcanvas"></button>
+                    </div>
+                    <div class="offcanvas-body">
+                        <ul class="navbar-nav justify-content-end flex-grow-1 text-center">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="#">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Veranstaltungen</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Community</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Kontakt</a>
+                            </li>
+                        </ul>
+                        
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <div class="header_height"></div>
+
+
+
