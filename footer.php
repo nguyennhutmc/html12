5839307bf5696d6ec0fafe23358e87cadad619e4
Nhut Nguyen <nhutnguyen@vba-adk.com>
2025-06-25 09:27:15 +0700

    u

---
+++ footer.php
@@ -0,0 +1,26 @@
+<footer>
+<div class="container">
+            <div class="row g-4 g-lg-5">
+                <div class="col-lg-5 col-left position-relative pb-4">
+                    <img class="logo_xanh mb-3" src="images/logo_xanh.svg" alt="Footer Logo">
+                    <p>Ein Projekt der Beschriebungstext. Rae si volecus impor alictis duntia comnim volupta et pliciti nverem quiscienet mo illiqu.</p>
+                </div>
+                <div class="col-lg-4 offset-lg-1 korotdong col-center">
+                    <p>Straße 50, 99999 Berlin</p>
+                    <p>Tel.: 00 000 000 000</p>
+                    <p>Fax: 00 000 000 000</p>
+                    <p>E-Mail: emailadresse@e-mail.de</p>
+                </div>
+                <div class="col-lg-2 korotdong text-lg-end col-right">
+                    <p><a class="color-primary" href="#">Impressum</a></p>
+                    <p><a class="color-primary" href="#">Datenschutz</a></p>
+                </div>
+            </div>
+        </div>
+</footer>
+</div>
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="js/masonry.pkgd.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,227 @@
+<?php include 'header.php'; ?>
+<main class="home_page">
+   <section class="hero-section hero1">
+      <div class="container h-100 d-flex align-items-center justify-content-center">
+         <img class="logo_trang" src="images/logo_trang.svg" alt="">
+      </div>
+   </section>
+   <section class="section1 text-center">
+      <div class="container">
+         <h3 class="fw-semibold mb-1">ON WATER | WasserWissen in Berlin</h3>
+         <p>lädt Wissenschaftler*innen ein, ihre Wasserforschung im Austausch<br>
+            mit der Gesellschaft zu diskutieren.
+         </p>
+         <p>Das Programm bringt Forscher*innen von Humboldt Universität, Technische Universität, Freie Universität sowie
+            Charité Universitätsmedizin mit der Stadtgesellschaft und ihren Wasser- Expert*innen zusammen.
+         </p>
+         <p>Das stärkt nicht nur die exzellente Forschung im Verbund der Berlin University Alliance, sondern befördert
+            auch die Resilienz der Gesellschaft.
+         </p>
+      </div>
+   </section>
+   <section class="event-section">
+      <div class="container">
+         <div class="row">
+            <div class="col-lg-8 col-left">
+               <div class="row">
+                  <div class="col-lg-6">
+                     <div class="card">
+                        <div class="card-body vstack">
+                           <h3 class="card-title">Veranstaltung Beispiel</h3>
+                           <div class="card-text text_ellipsis">Lorem ipsum dolor sit amet, consectetuer adipiscing elit sed ...</div>
+                           <a href="#" class="seemore stretched-link">Weiter Informationen<img class="icon_right ms-4" src="images/right.svg" alt=""></a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="card">
+                        <div class="card-body vstack">
+                           <h3 class="card-title">Headline für eine weitere Veranstaltung Beispiel</h3>
+                           <div class="card-text text_ellipsis">Cras risus arcu, sodales ac, ultrices ac, mollis quis, justo. Seda libero...</div>
+                           <a href="#" class="seemore stretched-link">Weiter Informationen<img class="icon_right ms-4" src="images/right.svg" alt=""></a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="card">
+                        <div class="card-body vstack">
+                           <h3 class="card-title">Veranstaltung</h3>
+                           <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibendum auci ...</div>
+                           <a href="#" class="seemore stretched-link">Weiter Informationen<img class="icon_right ms-4" src="images/right.svg" alt=""></a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-lg-6">
+                     <div class="card">
+                        <div class="card-body vstack">
+                           <h3 class="card-title">Veranstaltung</h3>
+                           <div class="card-text text_ellipsis">Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi ...</div>
+                           <a href="#" class="seemore stretched-link">Weiter Informationen<img class="icon_right ms-4" src="images/right.svg" alt=""></a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <div class="col-lg-4 col-right">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="card">
+                        <div class="card-body pe-lg-0">
+                           <h2 class="card-title">News & Veranstaltungen</h2>
+                           <div class="card-text">Rae si volecus impor alictis duntia comnim volupta ssequi apis nonsequis samet pliciti nverem quiscienet mo illiqu.</div>
+                           <a href="#" class="btn btn-primary">Überblick</a>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="custom-section">
+      <div class="container">
+         <div class="row gy-4">
+            <div class="col-lg-6 col-left pe-lg-5">
+               <div class="pe-lg-5">
+                  <img class="logo_xanh" src="images/logo_xanh.svg" alt="">
+                  <h2 class="mb-3">Du bist ein Teil von uns</h2>
+                  <p>Womit sollte sich die Wasserforschung befassen? Teile deine Ideen und Anregungen mit uns und werde Teil unserer Community! (max. 220 Zeichen)</p>
+               </div>
+            </div>
+            <div class="col-lg-6 col-right">
+               <div class="contact-form">
+                  <form>
+                     <div class="row g-4">
+                        <div class="col-lg-6"><input type="text" class="form-control" placeholder="Name"></div>
+                        <div class="col-lg-6"><input type="text" class="form-control" placeholder="Phone"></div>
+                        <div class="col-lg-12"><textarea class="form-control" rows="4" placeholder="Message"></textarea></div>
+                        <div class="col-lg-12"><button type="submit" class="btn btn-primary">Absenden</button></div>
+                     </div>
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="tintuc">
+      <div class="container">
+         <div class="row" data-masonry='{"percentPosition":true,"horizontalOrder": true}'>
+            <div class="col-lg-4">
+               <div class="card height1 background1">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height2 background2">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height3 background3">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height2 background4">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height3 background3">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height1 background4">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height3 background2">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height1 background3">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+            <div class="col-lg-4">
+               <div class="card height2 background1">
+                  <a class="card-body vstack">
+                     <div class="card-text text_ellipsis">Nibh vel velit auctor aliqunean sollicitudin lorem bibeum auci Nibh vel velit auctor aliqunean sollicitudinlorem bibendum Nibh vel velit auctor aliqunean.</div>
+                     <p class="mt-auto">Name Nachname, Ort, Stadtteil</p>
+                  </a>
+               </div>
+            </div>
+         </div>
+      </div>
+   </section>
+   <section class="hero-section hero2">
+      <div class="container h-100 d-flex align-items-center justify-content-center">
+         <img class="logo_trang" src="images/logo_trang.svg" alt="">
+      </div>
+   </section>
+   <section class="container gallery-section">
+      <h2 class="mb-3">Unsere Event-Galerie</h2>
+      <div class="col-lg-6 pe-lg-1 mb-4 pb-2">
+         <p>Erlebe die schönsten Momente unserer vergangenen Veranstaltungen!
+            In unserer Galerie findest du viele Eindrücke, Highlights und besondere
+            Augenblicke, die wir gemeinsam erlebt haben. Viel Spaß beim Stöbern!
+         </p>
+      </div>
+      <div class="row g-0">
+         <div class="col-lg-6 col-12">
+            <img class="img1 img" src="images/gallery-image1.jpeg" alt="">
+         </div>
+         <div class="col-lg-3 col-6">
+            <div class="row">
+               <div class="col-12"><img class="img2 img" src="images/gallery-image2.jpeg" alt=""></div>
+               <div class="col-12"><img class="img3 img" src="images/gallery-image3.jpeg" alt=""></div>
+            </div>
+         </div>
+         <div class="col-lg-3 col-6">
+            <img class="img4 img" src="images/gallery-image4.jpeg" alt="">
+         </div>
+         <div class="col-lg-5 col-12">
+            <div class="row">
+               <div class="col-lg-12 col-12"><img class="img5 img" src="images/gallery-image5.jpeg" alt=""></div>
+               <div class="col-lg-6 col-6">
+                  <img class="img6 img" src="images/gallery-image6.jpeg" alt="">   
+               </div>
+               <div class="col-lg-6 col-6">
+                  <img class="img7 img" src="images/gallery-image7.jpeg" alt="">
+               </div>
+            </div>
+         </div>
+         <div class="col-lg-7 col-12">
+            <img class="img8 img" src="images/gallery-image8.jpeg" alt="">
+         </div>
+      </div>
+   </section>
+</main>
+<?php include 'footer.php'; ?>
