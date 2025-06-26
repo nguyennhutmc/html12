60ad04c5cda0725b6865515f094ff3fcdb8fbeb8
Nhut Nguyen <nhutnguyen@vba-adk.com>
2025-06-26 13:30:06 +0700

    u

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="footer-top">
         <div class="container">
             <div class="col-lg-10 mx-auto">
-            <div><img class="logo_footer_1" src="images/logo_footer.png" alt=""></div>
+            <div class="mb-5"><img class="logo_footer_1" src="images/logo_footer.png" alt=""></div>
             <p>Gefördert vom Bundesministerium für Bildung und Forschung (BMBF) und dem Land<br>
                 Berlin im Rahmen der Exzellenzstrategie von Bund und Ländern</p>
             <div><img class="logo_footer_2" src="images/logo_footer_2.png" alt=""></div>
